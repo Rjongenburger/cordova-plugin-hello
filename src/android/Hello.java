2c9670dadac75e3e9230bfb221f1aa34be30eb1f
Richard Jongenburger <rjo@heycopsi.nl>
2019-10-21 12:45:09 +0200

    Add tone

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,6 +3,8 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 
 public class Hello extends CordovaPlugin {
 
@@ -10,7 +12,8 @@ public class Hello extends CordovaPlugin {
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
-
+            ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+            toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP,150);
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
